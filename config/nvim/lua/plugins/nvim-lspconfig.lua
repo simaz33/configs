fb6ecfc51c799016e7c562e429dfb992a73f7bd9
simaz33 <simonasrr@gmail.com>
2026-03-27 19:55:23 +0200

    chore: update nvim config

--- config/nvim/lua/plugins/nvim-lspconfig.lua
+++ config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,11 +1,5 @@
 return {
     {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local capabilities = require('blink.cmp').get_lsp_capabilities()
-            local lspconfig = require('lspconfig')
-
-            lspconfig['terraformls'].setup({ capabilities = capabilities })
-        end
-    },
+        "neovim/nvim-lspconfig"
+    }
 }
